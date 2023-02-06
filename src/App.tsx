48445676197fe4327a7ff98ffcc5b85065418004
Brice Boualavong <brice.boualavong@epitech.eu>
2023-02-06 15:55:39 +0100

    [ADD] forms finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TextField, Button, Table, TableBody, TableRow, TableCell } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Define an interface for the form data
+interface FormData {
+  name: string;
+  description: string;
 }
 
-export default App;
+const FormPage: React.FC = () => {
+  // State for the form data
+  const [formData, setFormData] = useState<FormData[]>([]);
+
+  // State for the form fields
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  // Handle the form submission
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Add the current form data to the formData state
+    setFormData([...formData, { name, description }]);
+
+    // Reset the form fields
+    setName('');
+    setDescription('');
+  };
+
+  // Handle the deletion of a form from the table
+  const handleDelete = (index: number) => {
+    // Filter the form data to remove the specified form
+    setFormData(formData.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      {/* The form */}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+        <TextField
+          label="Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          required
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      {/* The table */}
+      <Table>
+        <TableBody>
+          {formData.map((data, index) => (
+            <TableRow key={index}>
+              {/* Display the name */}
+              <TableCell>{data.name}</TableCell>
+              {/* Display the description */}
+              <TableCell>{data.description}</TableCell>
+              {/* Delete button */}
+              <TableCell>
+                <Button onClick={() => handleDelete(index)}>Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default FormPage;
